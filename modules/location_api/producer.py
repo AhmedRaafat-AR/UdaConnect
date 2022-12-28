85ae12fd84737c7c065a5c469186f9e28072575f
Ahmed Raafat <ar.ud.fw@gmail.com>
2022-12-28 04:37:52 +0200

    fix the typo KAFKA

--- modules/location_api/producer.py
+++ modules/location_api/producer.py
@@ -2,7 +2,7 @@ import os
 from kafka import KafkaProducer
 import json 
 
-kafka_server = os.environ["Kafka_API"]
+kafka_server = os.environ["KAFKA_API"]
 kafka_topic = os.environ["KAFKA_TOPIC"]
 
 producer = KafkaProducer(bootstrap_servers=kafka_server)
