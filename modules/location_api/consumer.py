c48381913e100f94078877a96f9d2e17648b9539
Ahmed Raafat <ar.ud.fw@gmail.com>
2022-12-29 14:46:02 +0200

    update consumer file & add msg file

--- modules/location_api/consumer.py
+++ modules/location_api/consumer.py
@@ -7,14 +7,17 @@ import json
 #kafka_topic = "locations"
 
 # configmap
-kafka_server = os.environ["KAFKA_API"]
-kafka_topic = os.environ["KAFKA_TOPIC"]
+# kafka_server = os.environ["KAFKA_API"]
+# kafka_topic = os.environ["KAFKA_TOPIC"]
 
-consumer = KafkaConsumer(kafka_topic, bootstrap_servers=kafka_server)
+consumer = KafkaConsumer("locations", bootstrap_servers="localhost:9092")
+
+msg = open(r"msg", "w")
 
 def Read(consumer):
     for location in consumer:
         loc = json.loads(location.value.decode('utf-8'))
-        print (loc)
+        print(loc)
+        return msg.write(f"{loc}")
 
 Read(consumer)    
