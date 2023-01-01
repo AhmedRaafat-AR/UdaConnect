c7de89d210582aa5baedd1d7e7925438c17e87b8
Ahmed Raafat <ar.ud.fw@gmail.com>
2023-01-01 23:29:11 +0200

    update location api files & deployment

--- modules/location_api/consumer_to_db.py
+++ modules/location_api/consumer_to_db.py
@@ -1,27 +1,20 @@
 import logging
 from geoalchemy2.functions import ST_Point
-from ast import Dict
-from models import Location
-from schemas import LocationSchema
-from geoalchemy2.functions import ST_Point
-from db_engine import db_engine
+from datetime import datetime
+import db_engine
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("location-api")
 
-def consumer_to_db(location_message):
-    @staticmethod
-    def create(location: Dict) -> Location:
-        validation_results: Dict = LocationSchema().validate(location)
-        if validation_results:
-            logger.warning(f"Unexpected data format in payload: {validation_results}")
-            raise Exception(f"Invalid payload: {validation_results}")
+def create(loc_message):
+    coordinate = ST_Point(
+        loc_message['latitude'], loc_message['longitude'])
 
-        new_location = Location()
-        new_location.person_id = location["userId"]
-        new_location.creation_time = location["creation_time"]
-        new_location.coordinate = ST_Point(location["latitude"], location["longitude"])
-        db_engine.db.session.add(new_location)
-        db_engine.db.session.commit()
+    add = db_engine.locations.insert().values(
+        person_id=loc_message['userId'],
+        creation_time=datetime.utcnow(),
+        coordinate=coordinate
+    )
 
-        return new_location
+    with db_engine.db.begin() as conn:
+        conn.execute(add)
--- modules/location_api/db_engine.py
+++ modules/location_api/db_engine.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 
 DB_USERNAME = os.environ["DB_USERNAME"]
@@ -8,7 +8,11 @@ DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 
-class db_engine():
-    db = create_engine(
-        f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
+
+db = create_engine(
+    f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+metadata_obj = MetaData()
+metadata_obj.reflect(bind=db)
+locations = metadata_obj.tables['location']
